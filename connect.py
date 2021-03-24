ab890baeddd47b32d17f8fe3f882eb25925c1bbe
Yara161 <66169990+Yara161@users.noreply.github.com>
2021-03-24 20:09:11 +0100

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,36 @@
+import json
+import pymongo
+from pymongo import MongoClient
+import pandas as pd
+data={}
+import redis
+import time
+
+r= redis.Redis(host= 'redis', port=6379, db=0)
+
+while True: #nog per minuut
+    time.sleep(60 - time.time() % 60)
+    print("yes")
+
+    client = pymongo.MongoClient(host= 'mongo' , port=27017)
+    mydb = client["Bitcoin"]
+    mycol = mydb["Transaction_info"]  
+    test=r.get('test_json')
+    testdic=json.loads(test)
+    # print(type(testdic))
+    print("wtf")
+
+    dictionary= dict(sorted(testdic.items(),key=lambda r:r[1][2],reverse=True))
+    # print(dictionary)
+
+    hash=next(iter(dictionary))
+    hoogstewaarde=str(next(iter(dictionary.values())))
+    #print(type(hoogstewaarde))
+
+    eind={hash:hoogstewaarde}
+    print(eind)
+
+
+    x = mycol.insert_one(eind)
+    print(x)
+    r.expire('test_json',60)
