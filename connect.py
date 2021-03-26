3a844114edfa539c837ea49e5780556a4a12a87c
Yara161 <66169990+Yara161@users.noreply.github.com>
2021-03-26 16:44:36 +0100

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,38 @@
+import json
+import pymongo
+from pymongo import MongoClient
+import pandas as pd
+data={}
+import redis
+import time
+
+
+r= redis.Redis(host= 'redis', port=6379, db=0)
+
+client = pymongo.MongoClient('mongodb://mongo:27017')
+mydb = client["Bitcoin"]
+mycol = mydb["Transaction_info"] 
+while True: #nog per minuut
+    try:
+        test=r.get('test_json')
+        testdic=json.loads(test)
+        # print(type(testdic))
+        dictionary= dict(sorted(testdic.items(),key=lambda r:r[1][2],reverse=True))
+        # print(dictionary)
+
+        hash=next(iter(dictionary))
+        hoogstewaarde=str(next(iter(dictionary.values())))
+        #print(type(hoogstewaarde))
+
+        eind={hash:hoogstewaarde}
+        print(eind)
+
+
+        x = mycol.insert_one(eind)
+        print(x)
+        r.delete('test_json')
+        testdic=[]
+        dictionary=[]
+        print("deleted")
+    except:
+        print()
